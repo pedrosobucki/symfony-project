8e9397374a00ea272c4f33850e33a350ecb785f2
pedrosobucki <pedrosobucki@gmail.com>
2022-06-08 11:03:31 -0300

    saves doctors in sqlite database and retrieves all in GET route

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -3,12 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\Doctor;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DcotorController {
+class DoctorController extends AbstractController{
+
+  private $entityManager;
+  private $doctrine;
+
+  public function __construct(EntityManagerInterface $entityManager, ManagerRegistry $doctrine) {
+    $this->entityManager = $entityManager;
+    $this->doctrine = $doctrine;
+  }
+
+  private function getDoctrine() : ManagerRegistry {
+    return $this->doctrine;
+  }
 
   /**
    * @Route("/doctors", methods="POST")
@@ -20,7 +35,22 @@ class DcotorController {
     $doctor->setCrm($content->crm);
     $doctor->setName($content->name);
 
+    $this->entityManager->persist($doctor);
+    $this->entityManager->flush();
+
     return new JsonResponse($doctor);
+  }
+
+  /**
+   * @Route("/doctors", methods="GET")
+   */
+  public function getAall() : Response {
+
+    $doctorRespository = $this->getDoctrine()->getRepository(Doctor::class);
+
+    $doctors = $doctorRespository->findAll();
+
+    return new JsonResponse($doctors);
   }
 }
 ?>
--- src/Entity/Doctor.php
+++ src/Entity/Doctor.php
@@ -2,10 +2,37 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
 class Doctor {
 
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(type="integer")
+   */
+  public int $id;
+
+  /**
+   * @ORM\Column(type="integer")
+   */
   public int $crm;
+  
+  /**
+   * @ORM\Column(type="string")
+   */
   public string $name;
+
+  public function getId() : int {
+    return $this->id;
+  }
+
+  public function setId(int $id) : void {
+    $this->id = $id;
+  }
 
   public function getCrm() : int {
     return $this->crm;
