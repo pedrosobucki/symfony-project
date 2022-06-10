eb836d0433dbf4c9f895b8b5c1e833abf295192f
pedrosobucki <pedrosobucki@gmail.com>
2022-06-10 10:13:46 -0300

    creation of all CRUD routes for manipulation of expertise data

--- src/Controller/ExpertiseController.php
+++ src/Controller/ExpertiseController.php
@@ -23,6 +23,7 @@ class ExpertiseController extends AbstractController{
         $this->entityManager = $entityManager;
         $this->repository = $repository;
     }
+
     /** 
      *@Route("/expertises", methods="POST")
      */
@@ -35,6 +36,61 @@ class ExpertiseController extends AbstractController{
         $this->entityManager->persist($expertise);
         $this->entityManager->flush();
 
+        return new JsonResponse($expertise, Response::HTTP_CREATED);
+    }
+
+    /** 
+     *@Route("/expertises", methods="GET")
+     */
+    public function getAll() : Response {
+        $expertises = $this->repository->findAll();
+        return new JsonResponse($expertises);
+    }
+
+    /** 
+     *@Route("/expertises/{id}", methods="GET")
+     */
+    public function getById(int $id) : Response {
+        $expertise = $this->repository->find($id);
+
+        if(is_null($expertise))
+            return new JsonResponse('', Response::HTTP_NO_CONTENT);
+
         return new JsonResponse($expertise);
+    }
+
+    /** 
+     *@Route("/expertises/{id}", methods="PUT")
+     */
+    public function update(int $id, Request $request) : Response {
+        $content = json_decode($request->getContent());
+
+        $newExpertise = new Expertise();
+        $newExpertise->setDescription($content->description);
+
+        $fetchedExpertise = $this->repository->find($id);
+        if(is_null($fetchedExpertise))
+            return new JsonResponse('', Response::HTTP_NOT_FOUND);
+
+        $fetchedExpertise->setDescription($newExpertise->getDescription());
+
+        $this->entityManager->flush();
+
+        return new JsonResponse($fetchedExpertise);
+    }
+
+    /** 
+     *@Route("/expertises/{id}", methods="DELETE")
+     */
+    public function delete(int $id) : Response {
+
+        $expertise = $this->repository->find($id);
+        if(is_null($expertise))
+            return new JsonResponse('', Response::HTTP_NOT_FOUND);
+
+        $this->entityManager->remove($expertise);
+        $this->entityManager->flush();
+
+        return new JsonResponse('', Response::HTTP_NO_CONTENT);
     }
 }
